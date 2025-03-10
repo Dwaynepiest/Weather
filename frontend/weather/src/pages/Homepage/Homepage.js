1372f445c9cfa9899244712da5ff9282ff58c772
Lucy Hoenders <lucyhoenders@gmail.com>
2025-03-10 18:13:08 +0200

    react

---
+++ frontend/weather/src/pages/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+      <div>
+        <h1>Welkom op de Homepagina!</h1>
+      </div>
+    );
+  };
+
+
+export default Homepage;
