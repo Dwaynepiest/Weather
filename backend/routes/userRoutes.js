50fb2ab8b4424b8d3b4272979528bb6b5eb21916
Dwaynepiest <dwaynepiest@outlook.com>
2025-03-11 11:50:18 +0200

    Elektriciteit backend toegevoegd, kijken naar de goede url voor de berekening te maken.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -104,136 +104,30 @@ router.post('/login', apiKeyMiddleware, async (req, res) => {
 });
 
 
-router.put('/:id', apiKeyMiddleware, async (req, res) => { 
-  const { id } = req.params;
-  const { 
-    email, 
-    zip_code, 
-    gender, 
-    relation, 
-    preference, 
-    one_liner, 
-    job, 
-    education, 
-    hobby, 
-    about_you, 
-    foto, 
-    old_password, 
-    new_password, 
-    confirm_password 
+router.put('/:user_id', apiKeyMiddleware, async (req, res) => {
+  const { user_id } = req.params;
+  const {
+    first_name,
+    surname,
+    email,
+    old_password,
+    new_password,
+    confirm_new_password,
+    birthdate,
   } = req.body;
 
-  // Haal de bestaande gebruikergegevens op
-  db.query('SELECT * FROM users WHERE user_id = ?', [id], async (err, results) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    if (results.length === 0) {
-      return res.status(404).send('Account niet gevonden.');
-    }
+  // Voeg hier je logica toe om de gebruiker bij te werken met de user_id
+  // Bijvoorbeeld:
+  // const user = await User.findById(user_id);
+  // if (!user) return res.status(404).send('User not found.');
+  // user.first_name = first_name;
+  // user.surname = surname;
+  // user.email = email;
+  // etc.
 
-    const user = results[0];
-
-    // Controleer of het e-mailadres wordt bijgewerkt en of het al bestaat
-    if (email && email !== user.email) {
-      db.query('SELECT * FROM users WHERE email = ?', [email], (err, emailResults) => {
-        if (err) {
-          return res.status(500).send('Databasefout bij het controleren van e-mail.');
-        }
-
-        if (emailResults.length > 0) {
-          return res.status(400).send('Een account met dit e-mailadres bestaat al.');
-        }
-
-        // Als het e-mailadres geldig is, ga door met bijwerken
-        updateUserData();
-      });
-    } else {
-      // Als het e-mailadres niet wordt bijgewerkt of het is geldig, ga door met bijwerken
-      updateUserData();
-    }
-
-    // Functie om de gebruiker bij te werken
-    async function updateUserData() {
-      // Als het wachtwoord wordt bijgewerkt, controleren of het oude wachtwoord correct is en het nieuwe wachtwoord is bevestigd
-      let updatedPassword = user.password;
-      if (new_password) {
-        if (old_password) {
-          // Vergelijk het oude wachtwoord met het opgeslagen wachtwoord
-          const isMatch = await bcrypt.compare(old_password, user.password);
-          if (!isMatch) {
-            return res.status(400).send('Het oude wachtwoord is incorrect.');
-          }
-        } else {
-          return res.status(400).send('Je moet je oude wachtwoord invoeren.');
-        }
-
-        // Controleer of de nieuwe wachtwoorden overeenkomen
-        if (new_password !== confirm_password) {
-          return res.status(400).send('De nieuwe wachtwoorden komen niet overeen.');
-        }
-
-        // Valideer wachtwoordsterkte
-        if (!validatePassword(new_password)) {
-          return res.status(400).send('Wachtwoord voldoet niet aan de vereisten.');
-        }
-
-        // Hash het nieuwe wachtwoord
-        updatedPassword = await bcrypt.hash(new_password, 10);
-      }
-
-      // Update alleen de velden die zijn meegegeven, behalve accept_service, birth_date, en nickname
-      const updatedUser = {
-        email: email || user.email,
-        zip_code: zip_code || user.zip_code,
-        gender: gender || user.gender,
-        relation: relation || user.relation,
-        preference: preference || user.preference,
-        one_liner: one_liner || user.one_liner,
-        job: job || user.job,
-        education: education || user.education,
-        hobby: hobby || user.hobby,
-        about_you: about_you || user.about_you,
-        foto: foto || user.foto,
-        password: updatedPassword, // Zet het gehashte wachtwoord als het is bijgewerkt
-        accept_service: user.accept_service, // Bewaar de oude waarde van accept_service
-        birth_date: user.birth_date, // Bewaar de oude waarde van birth_date
-        nickname: user.nickname // Bewaar de oude waarde van nickname
-      };
-
-      // Bijwerken van de gebruiker in de database
-      db.query(
-        'UPDATE users SET email = ?, zip_code = ?, gender = ?, relation = ?, preference = ?, one_liner = ?, job = ?, education = ?, hobby = ?, about_you = ?, foto = ?, password = ? WHERE user_id = ?',
-        [
-          updatedUser.email, 
-          updatedUser.zip_code, 
-          updatedUser.gender, 
-          updatedUser.relation, 
-          updatedUser.preference, 
-          updatedUser.one_liner, 
-          updatedUser.job, 
-          updatedUser.education, 
-          updatedUser.hobby, 
-          updatedUser.about_you, 
-          updatedUser.foto, 
-          updatedUser.password, 
-          id
-        ],
-        (err, updateResults) => {
-          if (err) {
-            return res.status(500).send(err);
-          }
-
-          res.json({
-            message: 'Account succesvol bijgewerkt',
-            id,
-            ...updatedUser,
-          });
-        }
-      );
-    }
-  });
+  res.send('User updated successfully');
 });
+
 
 
 router.delete('/:user_id', apiKeyMiddleware, async (req, res) => {
